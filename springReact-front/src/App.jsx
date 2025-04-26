64a47a2e23c5f35e5fdc9a75d2da3f5dbda49cf3
K4u4Guimaraes <kauaguimaraes0001@gmail.com>
2025-04-26 00:13:30 -0300

    importing BrowserRouter components and updating nodeJS

--- springReact-front/src/App.jsx
+++ springReact-front/src/App.jsx
@@ -1,15 +1,22 @@
 import './App.css'
 import FooterComponents from './components/FooterComponents'
-
 import HeaderComponents from './components/HeaderComponents'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import {BrowserRouter, Routes,Route} from 'react-router-dom'
 
 function App(){
   return(
     <>
-      <HeaderComponents/>
-      <ListEmployeeComponent/>
-      <FooterComponents/>
+      <BrowserRouter>
+        <HeaderComponents/>
+
+        <Routes>
+          <Route path='/' element={<ListEmployeeComponent/>}></Route>
+          <Route path=''></Route>
+        </Routes>
+        
+        <FooterComponents/>
+      </BrowserRouter>
       
     </>
   )
